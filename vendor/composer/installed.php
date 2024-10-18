e4ed22fb7b98958581d75a77c66b900ab3c9a6cc
Woo <info@woo.com>
2024-10-18 15:24:19 +0000

    Updates to 6.1.0

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'woocommerce/automatewoo',
+        'pretty_version' => 'dev-release/6.1.0',
+        'version' => 'dev-release/6.1.0',
+        'reference' => '6da258e42b000e8721d6b0e370d96e4da32d57dd',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'woocommerce/automatewoo' => array(
+            'pretty_version' => 'dev-release/6.1.0',
+            'version' => 'dev-release/6.1.0',
+            'reference' => '6da258e42b000e8721d6b0e370d96e4da32d57dd',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
